48974815579ec6f6baf9640509c026c5a2ee5aff
bozzeta <bozz202210@gmail.com>
2024-04-12 04:44:06 +0800

    feat: init template

---
+++ app/routes/_index.jsx
@@ -0,0 +1,10 @@
+export const meta = () => {
+  return [
+    { title: "New Remix SPA" },
+    { name: "description", content: "Welcome to Remix (SPA Mode)!" },
+  ];
+};
+
+export default function Index() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
